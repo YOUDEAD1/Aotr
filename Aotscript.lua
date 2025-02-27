da7c6edbf0995c2913506eeb1899ab2051848a97
YOUDEAD1 <99083420+YOUDEAD1@users.noreply.github.com>
2025-02-27 16:12:52 +0000

    Update Aotscript.lua

--- Aotscript.lua
+++ Aotscript.lua
@@ -1,4 +1,4 @@
--- تعريف الخدمات
+-- تعريف الخدما
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
